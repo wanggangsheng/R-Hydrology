dcaf3f5e8b4d5f82737e8c1bbaedee93ba670536
Wang Gangsheng <wang.gangsheng@gmail.com>
2021-03-17 17:17:29 +0800

    add Xin'AnJiang model

---
+++ XinAJ/XinAJ.R
@@ -0,0 +1,319 @@
+## Xin'AnJiang Hydrologic Model
+## Modified from C++ code in 'Sibada/XAJ'
+## (Errors exist in 'Sibada/XAJ': runoff routing by unit hydrograph)
+## Gangsheng Wang (wang.gangsheng@gmail.com)
+## March 15, 2021
+
+XinAJrun <- function(PREC, EVAP, parameters, UH, Area, dt) {
+  ## Area [km^2]: watershed area
+  # /* **************************************************************************
+  #   * Model parameters.
+  # ************************************************************************* */
+  nt <- length(PREC) ## Number of steps
+  
+  i <- 0
+  j <- 0
+  
+  KC <- parameters[1]          ## 1.  Ratio of potential evap to pan evap
+  IM <- parameters[2]          ## 2.  Fraction of impermeable area
+  WUM <- parameters[3]         ## 3.  Soil moisture capacity of upper layer
+  WLM <- parameters[4]         ## 4.  Soil moisture capacity of lower layer
+  WDM <- parameters[5]         ## 5.  Soil moisture capacity of deep layer
+  C <- parameters[6]           ## 6.  Coefficient of deep evap
+  B <- parameters[7]           ## 7.  Exponent of the soil moisture storage capacity curve
+  
+  SM <- parameters[8]          ## 8.  Areal mean free water capacity of the surface soil layer
+  EX <- parameters[9]          ## 9.  Exponent of the free water capacity curve
+  KI <- parameters[10]          ## 10. outflow coefficients of the free water storage to groundwater
+  KG <- parameters[11]         ## 11. outflow coefficients of the free water storage to interflow
+  
+  CI <- parameters[12]         ## 12. recession constant of groundwater storage
+  CG <- parameters[13]         ## 13. recession constant of the lower interflow storage
+  
+  WM <- WUM + WLM + WDM        ## Mean water sotrage of the basin
+  WMM <- WM * (1. + B) / (1. - IM)   ## Maximum water storage in the basin
+  
+  # /** *************************************************************************
+  #   * Output and process variables.
+  # ************************************************************************* */
+  ##int nvars = 16
+  ##NumericMatrix out(nt, nvars)
+  ## Simulated Variabes
+  E_s <- rep(0, nt)    ## Total ET (mm)
+  EU_s <- rep(0, nt)   ## ET of upper soil layer
+  EL_s <- rep(0, nt)   ## ET of lower soil layer
+  ED_s <- rep(0, nt)   ## ET of deep soil layer
+  W_s <- rep(0, nt)    ## Total soil water/moisture (mm)
+  WU_s <- rep(0, nt)   ## soil water moisture of upper soil layer
+  WL_s <- rep(0, nt)   ## soil water moisture of lower soil layer
+  WD_s <- rep(0, nt)   ## soil water moisture of deep soil layer
+  R_s <- rep(0, nt)     ## Total runoff (mm) of each time step
+  RS_s <- rep(0, nt)    ## Surface runoff (mm) of each time step
+  RI_s <- rep(0, nt)    ## Interflow (mm) of each time step
+  RG_s <- rep(0, nt)    ## Underground runoff (mm) of each time step
+  Q_s <- rep(0, nt)     ## Total runoff (m^3/s) at the outlet of the basin
+  QS_s <- rep(0, nt)    ## Surface runoff (m^3/s) at the outlet of the basin
+  QI_s <- rep(0, nt)    ## Interflow runoff (m^3/s) at the outlet of the basin
+  QG_s <- rep(0, nt)    ## Underground runoff (m^3/s) at the outlet of the basin
+  
+  U <- Area / 3.6 / dt   ## Convert runoff from mm to m^3/s
+  
+  ## Model state variables for each step
+  PE <- 0.    ## net prec when > 0  insu evap when < 0
+  Ep <- 0.    ## Potential evapotranspiration, KC * EVAP[i]
+  P <- 0.
+  R <- 0.     ## Total runoff yield (mm)
+  RB <- 0.    ## Runoff yield of the impermeable area
+  RG <- 0.    ## Underground runoff yield
+  RI <- 0.    ## Interflow runoff yield
+  RS <- 0.    ## Surface runoff yield
+  A <- 0.     ##
+  E <- 0.     ## Total evap
+  EU <- 0.     ## Evap at upper soil layer
+  EL <- 0.     ## Evap at lower soil layer
+  ED <- 0.     ## Evap at deep soil layer
+  
+  FR <- 0.
+  AU <- 0.
+  WU <- WUM * 0.8       ## Soil moisture (mm) of upper layer
+  WL <- WLM * 0.8      ## Soil moisture (mm) of lower layer
+  WD <- WDM * 0.8      ## Soil moisture (mm) of deep layer
+  W <- WU + WL + WD    ## Soil moisture (mm) of all layer
+  
+  INUL <- 0.    ## Infiltration from upper layer to lower layer
+  INLD <- 0.    ## Infiltration from lower layer to deep layer
+  S <- SM * 0.2
+  
+  MS <- SM * (1. + EX)
+  tmpA <- 0.   ## temporary variable in calculation of free water
+  
+  # /** ************************************************************************
+  #   * Runoff yield
+  # ************************************************************************* */
+  for (i in 1:nt) {
+
+    # /**   Evaporation   */
+    RB <- PREC[i] * IM       ## RB: precipitation of the impermeable area
+    P <- PREC[i] * (1. - IM)
+    Ep <- KC * EVAP[i]
+    
+    if ((WU + P) >= Ep) {
+      EU <- Ep
+      EL <- 0
+      ED <- 0
+    } else if ((WU + P) < Ep) {
+      EU <- WU + P
+      ED <- 0.
+      if (WL >= (C * WLM)) {
+        EL <- (Ep - EU) * WL / WLM
+      } else if (WL >= C * (Ep - EU)) {
+        EL <- C * (Ep - EU)
+      } else if (WL < C * (Ep - EU)) {
+        EL <- WL
+        ED <- C * (Ep - EU) - EL
+      }
+      if (ED > WD) {
+        ED <- WD
+      }
+    } ## else if ((WU + P) < Ep)
+    
+    E <- EU + EL + ED
+    PE <- P - E
+    
+    # /**  Infiltration and runoff yeild */
+    if (PE <= 0) {
+      R <- 0.00
+      W <- W + PE
+    } else {
+      # /**  Use soil water capacity curve to cal change of soil watet change */
+      A <- WMM * (1 - '^'((1.0 - W / WM), 1. / (1. + B)))
+      
+      ## Depth of soil moisture + net prec < maximum soil water storage
+      if ((A + PE) < WMM) {
+        R <- PE - (WM - WM * '^'((1 - (PE + A) / WMM), (1 + B)) - W)
+      } else {
+        R <- PE - (WM - W)
+      }
+    }
+    
+    ## Infiltration
+    if (WU + P - EU - R <= WUM) {
+      WU = WU + (P - EU - R)
+      WL = WL - EL
+      WD = WD - ED
+      INUL <- 0.
+    } else {
+      WU <- WUM
+      INUL <- WU + P - EU - R - WUM
+      if (WL - EL + INUL <= WLM) {
+        WL <- WL - EL + INUL
+        WD <- WD - ED
+        INLD <- 0.
+      } else {
+        WL <- WLM
+        INLD <- WL + INUL - EL - WLM
+        if (WD - ED + INLD <= WDM) {
+          WD <- WD - ED + INLD
+        } else {
+          WD <- WDM
+        }
+      }
+    }
+    
+    W <- WU + WL + WD
+    R_s[i] <- R
+    
+    # /**  Runoff source division */
+    if (PE > 0.) {
+      FR <- R / PE
+      AU <- MS * (1 - '^'((1 - S / SM), 1 / (1 + EX)))
+      if (PE + AU < MS) {
+        tmpA <- SM * '^'((1 - (PE + AU) / MS), 1 + EX)
+      } else {
+        tmpA <- 0.
+      }
+      RS <- FR * (PE + S - SM + tmpA)
+      S <- SM - tmpA
+      RI <- FR * KI * S
+      RG <- FR * KG * S
+      S <- S * (1 - KI - KG)
+      RS = RS + RB
+    } else {
+      FR <- 1 - '^'((1 - W / WM), B / (1 + B))
+      RI <- FR * KI * S
+      RG <- FR * KG * S
+      S <- S * (1 - KI - KG)
+      RS <- RB
+    }
+    ## Rcout << i << '\t' << FR<< '\t' << RS << '\t' << RI<<'\t' << RG << '\n'   ## !
+    R <- RS + RI + RG
+    
+    # /** Save process variables */
+    R_s[i] <- R
+    RS_s[i] <- RS    ## Surface runoff (mm) of each time step
+    RI_s[i] <- RI    ## Interflow (mm) of each time step
+    RG_s[i] <- RG    ## Underground runoff (mm) of each time step
+    
+    E_s[i] <- E
+    EU_s[i] <- EU
+    EL_s[i] <- EL
+    ED_s[i] <- ED
+    W_s[i] <- W
+    WU_s[i] <- WU
+    WL_s[i] <- WL
+    WD_s[i] <- WD
+  } ## for (i in 1: nt)
+  
+  # /***************************************************************************
+  #   * Routing
+  # ************************************************************************* */
+  
+  ## Routing of surface runoff
+  nUH <- length(UH)
+  ## nconv
+  for (i in 1:nt) {
+    nconv <- nUH
+    if (nUH > i) {
+      nconv <- i
+    }
+    for (j in 1:nconv)
+    {
+      QS_s[i] = QS_s[i] + RS_s[i - j + 1] * UH[j] * U
+    }
+  }
+  
+  ## Routing of interflow and underground runoff
+  for (i in 2:nt)	{
+    QI_s[i] <- CI * QI_s[i - 1] + (1.0 - CI) * RI_s[i] * U
+    QG_s[i] <- CG * QG_s[i - 1] + (1.0 - CG) * RG_s[i] * U
+  }
+  
+  Q_s <- QS_s + QI_s + QG_s
+  
+  
+  ## Rcout << "Modeling complete.\n"
+  XinAJrun <-data.frame(
+      Esim = E_s,
+      EUsim = EU_s,
+      ELsim = EL_s,
+      EDsim = ED_s,
+      Wsim = W_s,
+      WUsim = WU_s,
+      WLsim = WL_s,
+      WDsim = WD_s,
+      Rsim = R_s,
+      RSsim = RS_s,
+      RIsim = RI_s,
+      RGsim = RG_s,
+      Qsim = Q_s,
+      QSsim = QS_s,
+      QIsim = QI_s,
+      QGsim = QG_s
+    )
+}
+
+##==============================================================================
+
+# Create IUH
+IUH <- function(N, NK, len) {
+  UH <- pgamma(seq(0, 100, length.out = len + 1),
+               N, scale = NK)
+  UH <- diff(UH)
+  UH <- UH/sum(UH)
+  UH
+}
+##==============================================================================
+
+XinAJpar <- function() {
+  ## KI + KG = 0.70
+  param_names <- c('KC', 'IM', 'WUM', 'WLM', 'WDM', 'C', 'B', 'SM',  'EX', 'KI', 'KG', 'CI', 'CG', 'N', 'NK')
+  param_value <- c(0.90, 0.03,  20.0,  60.0,  40.0, 0.15, 0.3, 30.0, 1.50, 0.40, 0.30, 0.80, 0.96, 4.0, 3.0)
+  param_lower <- c(0.80, 0.00,   5.0,  10.0,  10.0, 0.05, 0.1, 10.0, 0.50, 0.01, 0.01, 0.50, 0.95, 0.1, 1.0)
+  param_upper <- c(1.20, 0.05,  20.0,  90.0,  60.0, 0.20, 0.6, 60.0, 2.00, 0.70, 0.70, 0.90, 0.998,5.0, 6.0)
+
+  XinAJpar <- data.frame(param_names = param_names, param_value = param_value, 
+                         param_lower = param_lower, param_upper = param_upper)
+  ## The lumped XAJ model has 13 parameters, including:
+  #'
+  #'          KC,   Ratio of potential evap to pan evap
+  #'
+  #'          IM,   Fraction of impermeable area
+  #'
+  #'          WUM,  Soil moisture capacity of upper layer
+  #'
+  #'          WLM,  Soil moisture capacity of lower layer
+  #'
+  #'          WDM,  Soil moisture capacity of deep layer
+  #'
+  #'          C,    Coefficient of deep evap
+  #'
+  #'          B,    Exponent of the soil moisture storage capacity curve
+  #'
+  #'          SM,   Areal mean free water capacity of the surface soil layer
+  #'
+  #'          EX,   Exponent of the free water capacity curve
+  #'
+  #'          KI,   outflow coefficients of the free water storage to interflow
+  #'
+  #'          KG,   outflow coefficients of the free water storage to groundwater
+  #'
+  #'          CI,   recession constant of the lower interflow storage
+  #'
+  #'          CG,   recession constant of groundwater storage
+  #'
+  #'          If use the instantaneous unit hydrograph (IUH) of Nash for routing
+  #'          of surface runoff, should provided two other parameters:
+  #'
+  #'          N,    number of reservoirs in the instantaneous unit hydrograph
+  #'
+  #'          NK,   common storage coefficient in the instantaneous unit hydrograph
+}
+
+
+##-----------------------------------------------------------------------------
+R2 <- function(obs,sim){
+  obs_mean <- mean(obs)
+  R2 <-  1-sum((sim-obs)^2)/sum((obs-obs_mean)^2)
+}
+
+##-----------------------------------------------------------------------------
---
+++ XinAJ/XinAJ_test.R
@@ -0,0 +1,247 @@
+## Xin'AnJiang Hydrologic Model
+## Example code to conduct hydrologic modeling
+## Gangsheng Wang (wang.gangsheng@gmail.com)
+## March 15, 2021
+
+##------------------------------------------------------------------------------
+## check data only:
+# install.packages('devtools')
+# devtools::install_github('Sibada/XAJ')
+
+# library(XAJ)
+# data(SB)
+# write.table(SB, file="XinAJ_input.csv",sep="\t",row.names = F)
+##------------------------------------------------------------------------------
+
+##------------------------------------------------------------------------------
+library(ggplot2)
+library(ggpubr)  ## ggscatter
+#Clean up Environment
+rm(list = ls())
+setwd("/Users/wgs/ownCloud/Rcode/R-Hydrology/XinAJ")
+source("XinAJ.R")
+
+## Precipitation + Evaporation (pan evaporation or potential evapotranspiration)
+# input_data <- read.table("XinAJ_PE.csv", sep="\t",head=T)
+## Precipitation (mm) + Evaporation (mm) + Observed streamflow (m3/s)
+input_data <- read.table("XinAJ_PEQ.csv", sep="\t",head=T)
+head(input_data)
+nt <- nrow(input_data)
+
+XAJpar <- XinAJpar()
+str(XAJpar)
+
+param <- XAJpar$param_value
+npar <- length(param)
+##-----------------------------------------------------------------------------
+## Randomly Generate Parameter Values from their ranges 
+for (i in 1:npar) {
+  param[i] <- runif(1, min = XAJpar$param_lower[i], max = XAJpar$param_upper[i])
+}
+param[10] <- 0.70 - param[11]  ## KI = 0.70 - KG
+names(param) <- XAJpar$param_names
+##-----------------------------------------------------------------------------
+param
+
+dt <- 24 ## days?
+area <- 1000 ## watershed area, km^2
+UH <- IUH(param[14], param[15],dt)
+plot(UH, type="l",xlab="Time", ylab="Unit Hydrograph")
+points(UH)
+
+XAJ_test <- XinAJrun(input_data$PREC,input_data$EVAP,param,UH, area, dt)
+str(XAJ_test)
+
+plot(XAJ_test$Qsim,type="l", xlab="Time", ylab="Qsim")
+head(XAJ_test)
+mean_sim <- colMeans(XAJ_test)
+ETsim <- mean_sim[2:4]
+Rsim <- mean_sim[10:12]
+Qsim <- mean_sim[14:16]
+
+pie(ETsim,main="ET")
+pie(Rsim, main="Rsim")
+pie(Qsim, main="Qsim")
+
+## Save the Qsim as Qobs for model evaluation
+# input <- data.frame(input_data,XAJ_test$Qsim*runif(1,0.8,1.2))
+# write.table(round(input,digits=3), file="XinAJ_input2.csv",sep="\t",row.names = F)
+
+R2_XAJ <- R2(input_data$Qobs,XAJ_test$Qsim)
+R2_XAJ
+
+##-----------------------------------------------------------------------------
+## Monte Carlo simulations
+## Randomly Generate multiple sets of Parameter Values from their ranges 
+nrun <- 100
+param_sample <- matrix(nrow = nrun, ncol = npar)
+R2_XAJ <- rep(0.,nrun)
+for (i in 1:npar) {
+  param_sample[,i] <- runif(nrun, min = XAJpar$param_lower[i], max = XAJpar$param_upper[i])
+}
+colnames(param_sample) <- XAJpar$param_names
+param_sample[,10] <- 0.70 - param_sample[,11]  ## KI = 0.70 - KG
+head(param_sample)
+
+for(i in 1:nrun) {
+  print(paste0("No. of model run = ",i," of ",nrun))
+  UH <- IUH(param_sample[i,14], param_sample[i,15],dt)
+  XAJ_test <- XinAJrun(input_data$PREC,input_data$EVAP,param_sample[i,],UH, area, dt)
+  R2_XAJ[i] <- R2(input_data$Qobs,XAJ_test$Qsim)
+}
+
+##---------------------------------------------------------
+## empirical cumulative probability 
+cumulative_probability <- ecdf(R2_XAJ)
+
+plot(cumulative_probability, 
+     main="", xlim=c(-2,1),
+     xlab=bquote(R^bold("2")),ylab="Cumulative probability")
+axis(side=1, at=seq(0, 1, by=0.2))
+##---------------------------------------------------------
+
+max(R2_XAJ)
+iR2max <- which.max(R2_XAJ)
+iR2max
+
+## Re-run the model with the best parameters------------------------------------
+param <- param_sample[iR2max,]
+UH <- IUH(param_sample[iR2max,14], param_sample[iR2max,15],dt)
+XAJ_test <- XinAJrun(input_data$PREC,input_data$EVAP,param,UH, area, dt)
+R2_XAJ <- R2(input_data$Qobs,XAJ_test$Qsim)
+R2_XAJ
+
+##------------------------------------------------------------------------------
+## calculate mean values and ratios
+head(XAJ_test)
+mean_sim <- colMeans(XAJ_test)
+ETmean <- mean_sim[1]
+Rmean <- mean_sim[9]
+
+ETsim <- mean_sim[2:4]
+Rsim <- mean_sim[10:12]
+Qsim <- mean_sim[14:16]
+
+Pmean <- mean(input_data$PREC)
+PETmean <- mean(input_data$EVAP)
+ET_P <- ETmean/Pmean ## ratio of ET to Precipitation
+R_P <- Rmean/Pmean
+PET_P <- PETmean/Pmean
+ET_PET <- ETmean/PETmean
+
+df1 <- data.frame(ET_P = ET_P, R_P = R_P, PET_P = PET_P, ET_PET = ET_PET)
+rownames(df1) <- "Ratio"
+df1
+
+##------------------------------------------------------------------------------
+## Time-Series Plot
+
+size_axis <- 20
+size_title <- size_axis+2
+
+title1 <- bquote("Xin'AnJiang Model: "*R^2*" = "*.(round(R2_XAJ,digits=3)))
+
+xlab1 <- 'Time'
+ylab1 <- bquote('Streamflow ('*m^3*' '*s^"-1"*')')
+
+df <- data.frame(seq(1,nt),input_data$Qobs,XAJ_test$Qsim)
+legend <- c("Qobs","Qsim")
+colnames(df) <- c("Time",legend)
+
+color1 <- c("blue","red")
+
+sp1 <- ggplot(data=df, aes(x=Time)) + 
+  ggtitle(title1) +
+  xlab(xlab1) + ylab(ylab1) + 
+  geom_line(aes(y = Qobs, color=legend[1]), size=1) +
+  geom_line(aes(y = Qsim, color=legend[2]), size=1) + 
+  
+  scale_color_manual(name=NULL,values = color1,limits=legend) +
+  guides(col = guide_legend(nrow=1)) +
+  theme(
+    plot.title=element_text(hjust=0.5, size=size_title,face="bold"),
+    plot.subtitle=element_text(hjust=-0.03, size=size_title, face="bold"),
+    strip.text.x=element_blank(),
+    axis.text=element_text(size=size_axis),
+    axis.title=element_text(size=size_title,face="bold"),
+    legend.text=element_text(size=size_axis),
+    legend.title=element_text(size=size_axis),
+    legend.position=c(0.5,0.9)
+  )
+
+print(sp1)
+fn1 <- paste0("XAJ_eval.pdf")
+ggsave(filename=fn1, plot=sp1,width=50,height=20,units="cm", dpi=600)
+
+##------------------------------------------------------------------------------
+## scatter plot
+size_axis <- 20
+size_title <- size_axis+2
+size_regression <- size_axis/3
+
+xmax1 <- ceiling(max(df$Qobs)/2)*2
+xmax2 <- ceiling(max(df$Qsim)/2)*2
+ymax <- max(xmax1,xmax2)
+ylim1 <- c(0, ymax)
+
+title1 <- bquote("Xin'AnJiang Model: "*R^2*" = "*.(round(R2_XAJ,digits=3)))
+xlab1 <- bquote(bold("Observed Streamflow ")*bold(" ("*m^3*" "*s^bold("-1")*")"))
+ylab1 <- bquote(bold("Simulated Streamflow ")*bold(" ("*m^3*" "*s^bold("-1")*")"))
+
+sp2 <- ggscatter(df, x="Qobs",y="Qsim",color="blue",shape=21,size=3) +
+  geom_abline(intercept = 0, slope = 1) + 
+  coord_cartesian(xlim=ylim1, ylim=ylim1) +
+  scale_x_continuous(breaks=seq(ylim1[1],ylim1[2],10)) +
+  scale_y_continuous(breaks=seq(ylim1[1],ylim1[2],10)) +
+  ggtitle(title1) +
+  xlab(xlab1) +
+  ylab(ylab1) +
+  theme(
+    panel.border = element_rect(size=2, colour = "black",fill=NA),
+    plot.title = element_text(hjust =0.5,size=size_title),
+    axis.text=element_text(size=size_axis),
+    axis.title=element_text(size=size_title,face="bold")
+  )
+
+print(sp2)
+fn2 <- paste0("XAJ_eval_scatter.pdf")
+ggsave(filename=fn2, plot=sp2,width=30,height=30,units="cm", dpi=600)
+
+##------------------------------------------------------------------------------
+
+xlab1 <- 'Time'
+ylab1 <- 'Q'
+
+df <- data.frame(seq(1,length(XAJ_test$Qsim)),XAJ_test$Qsim,XAJ_test$QSsim,XAJ_test$QIsim,XAJ_test$QGsim)
+legend <- c("Qsim","QSsim","QIsim","QGsim")
+colnames(df) <- c("Time",legend)
+
+color1 <- c("red","cornflowerblue","blue","darkgreen")
+
+sp3 <- ggplot(data=df, aes(x=Time)) + 
+  ggtitle(title1) +
+  xlab(xlab1) + ylab(ylab1) + 
+  geom_line(aes(y = Qsim, color=legend[1]), size=1) +
+  geom_line(aes(y = QSsim, color=legend[2]), size=1) + 
+  geom_line(aes(y = QIsim, color=legend[3]), size=1) +
+  geom_line(aes(y = QGsim, color=legend[4]), size=1) +
+  
+  
+  scale_color_manual(name=NULL,values = color1,limits=legend) +
+  guides(col = guide_legend(nrow=1)) +
+  theme(
+    plot.title=element_text(hjust=0.5, size=size_title,face="bold"),
+    plot.subtitle=element_text(hjust=-0.03, size=size_title, face="bold"),
+    strip.text.x=element_blank(),
+    axis.text=element_text(size=size_axis),
+    axis.title=element_text(size=size_title,face="bold"),
+    legend.text=element_text(size=size_axis),
+    legend.title=element_text(size=size_axis),
+    legend.position=c(0.5,0.9)
+  )
+
+print(sp3)
+fn3 <- paste0("XAJ_Qsim.pdf")
+ggsave(filename=fn3, plot=sp3,width=50,height=20,units="cm", dpi=600)
+
+##------------------------------------------------------------------------------
